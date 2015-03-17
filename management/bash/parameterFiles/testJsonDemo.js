18d1865431be0f7f8d1a3739027cc891e064d9ff
TQ White II <tq@justkidding.com>
2015-03-17 11:23:43 -0500

    test script update

--- management/bash/parameterFiles/testJsonDemo.js
+++ management/bash/parameterFiles/testJsonDemo.js
@@ -2,7 +2,7 @@
     "input": [  
 
 
-{"source":"http://127.0.0.1:8001/uff/1.0/districts/Albany/schools/010/segments/Guardian?sendFlatSpecs=true","destination":"Guardians", "path":"Data"}
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Albany/schools/010/segments/Guardian?sendFlatSpecs=true","destination":"Guardians", "path":"Data"}
 
 
 ],
@@ -10,7 +10,7 @@
     "transform": [],
 
     "output": {
-        "type": "file",
+        "type": "mysql",
         "context": {
             "parentPath": "zzStudentPlans/Albany/",
             "header":true,
